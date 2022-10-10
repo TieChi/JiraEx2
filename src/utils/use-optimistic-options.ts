aa44f55bbe44b43280315c8aec9a10d696eb00e2
hanjun <654777646@qq.com>
2022-10-10 17:07:29 +0800

    add

---
+++ src/utils/use-optimistic-options.ts
@@ -0,0 +1,39 @@
+import { QueryKey, useQueryClient } from "react-query";
+
+export const useConfig = (
+  queryKey: QueryKey,
+  callback: (target: any, old?: any[]) => any[]
+) => {
+
+  const queryClient = useQueryClient();
+  return {
+    onSuccess: () => queryClient.invalidateQueries(queryKey),
+    // 这个异步疑似多余，本地数据更新之前的预修改，应该没有异步过程。
+    async onMutate(target: any) {
+      const previousItems = queryClient.getQueryData(queryKey);
+      queryClient.setQueryData(queryKey, (old?: any[]) => {
+        return callback(target, old);
+      });
+      return { previousItems };
+    },
+    onError(error: any, newItem: any, context: any) {
+      queryClient.setQueryData(queryKey, context.previousItems);
+    },
+  };
+};
+
+export const useDeleteConfig = (queryKey: QueryKey) =>
+  useConfig(
+    queryKey,
+    (target, old) => old?.filter((item) => item.id !== target.id) || []
+  );
+export const useEditConfig = (queryKey: QueryKey) =>
+  useConfig(
+    queryKey,
+    (target, old) =>
+      old?.map((item) =>
+        item.id === target.id ? { ...item, ...target } : item
+      ) || []
+  );
+export const useAddConfig = (queryKey: QueryKey) =>
+  useConfig(queryKey, (target, old) => (old ? [...old, target] : []));
