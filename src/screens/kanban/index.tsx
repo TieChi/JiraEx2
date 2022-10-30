7cc40c2a9a1974ca92d25017a1a9d025e8ed9d05
hanjun <654777646@qq.com>
2022-10-30 21:38:57 +0800

    feat: 面板和任务排序UI端

--- src/screens/kanban/index.tsx
+++ src/screens/kanban/index.tsx
@@ -9,30 +9,60 @@ import { CreateKanban } from "./create-kanban";
 import { KanbanColumn } from "./kanban-column";
 import { SearchPanel } from "./search-panel";
 import { TaskModal } from "./task-modal";
-import { useKanbansSearchParams, useProjectInUrl, useTasksSearchParams } from "./util";
+import {
+  useKanbansSearchParams,
+  useProjectInUrl,
+  useTasksSearchParams,
+} from "./util";
+import { DragDropContext } from "react-beautiful-dnd";
+import { Drag, Drop, DropChild } from "components/drag-and-drop";
 
 export const KanbanScreen = () => {
-  useDocumentTitle('看板列表')
+  useDocumentTitle("看板列表");
 
-  const {data: currentProject} = useProjectInUrl()
-  const {data: kanbans, isLoading: kanbanLoading} = useKanbans(useKanbansSearchParams())
-  const { isLoading: taskLoading } = useTasks(useTasksSearchParams())
-  const isLoading = kanbanLoading || taskLoading
-  return <ScreenContainer>
-    <h1>{currentProject?.name}看板</h1>
-    <SearchPanel />
-    {isLoading ? <Spin size={"large"} /> : <ColumnsContainer>
-      {
-        kanbans?.map(kanban => <KanbanColumn kanban={kanban} key={kanban.id} />)
-      }
-      <CreateKanban />  
-    </ColumnsContainer>}
-    <TaskModal />
-  </ScreenContainer>;
+  const { data: currentProject } = useProjectInUrl();
+  const { data: kanbans, isLoading: kanbanLoading } = useKanbans(
+    useKanbansSearchParams()
+  );
+  const { isLoading: taskLoading } = useTasks(useTasksSearchParams());
+  const isLoading = kanbanLoading || taskLoading;
+  return (
+    <DragDropContext onDragEnd={() => {}}>
+      <ScreenContainer>
+        <h1>{currentProject?.name}看板</h1>
+        <SearchPanel />
+        {isLoading ? (
+          <Spin size={"large"} />
+        ) : (
+          <ColumnsContainer>
+            <Drop
+              type={"COLUMN"}
+              direction={"horizontal"}
+              droppableId={"kanban"}
+            >
+              <DropChild style={{ display: "flex" }}>
+                {kanbans?.map((kanban, index) => (
+                  <Drag
+                    key={kanban.id}
+                    draggableId={"kanban" + kanban.id}
+                    index={index}
+                  >
+                    <KanbanColumn kanban={kanban} key={kanban.id} />
+                  </Drag>
+                ))}
+              </DropChild>
+            </Drop>
+            <CreateKanban />
+          </ColumnsContainer>
+        )}
+        <TaskModal />
+      </ScreenContainer>
+    </DragDropContext>
+  );
 };
 
-export const ColumnsContainer = styled.div`
+export const ColumnsContainer = styled("div")`
   display: flex;
   overflow-x: scroll;
   flex: 1;
-`
+`;
--- src/screens/kanban/kanban-column.tsx
+++ src/screens/kanban/kanban-column.tsx
@@ -1,74 +1,110 @@
-import React from "react"
-import { Kanban } from "types/kanban"
-import { useDeleteTask, useTasks } from "utils/task"
-import { useTaskTypes } from "utils/task-type"
-import { useKanbansQueryKey, useTasksModal, useTasksSearchParams } from "./util"
-import taskIcon from "assets/task.svg"
-import bugIcon from "assets/bug.svg"
-import styled from "@emotion/styled"
-import { Button, Card, Dropdown, Menu, Modal } from "antd"
-import { CreateTask } from "./create-task"
-import { Task } from "types/task"
-import { Mark } from "components/mark"
-import { useDeleteKanban } from "utils/kanban"
-import { Row } from "components/lib"
-
+import React from "react";
+import { Kanban } from "types/kanban";
+import { useDeleteTask, useTasks } from "utils/task";
+import { useTaskTypes } from "utils/task-type";
+import {
+  useKanbansQueryKey,
+  useTasksModal,
+  useTasksSearchParams,
+} from "./util";
+import taskIcon from "assets/task.svg";
+import bugIcon from "assets/bug.svg";
+import styled from "@emotion/styled";
+import { Button, Card, Dropdown, Menu, Modal } from "antd";
+import { CreateTask } from "./create-task";
+import { Task } from "types/task";
+import { Mark } from "components/mark";
+import { useDeleteKanban } from "utils/kanban";
+import { Row } from "components/lib";
+import { Drag, Drop, DropChild } from "components/drag-and-drop";
 
 const TaskTypeIcon = ({ id }: { id: number }) => {
-  const {data: taskTypes} = useTaskTypes()
-  const name = taskTypes?.find((taskType) => taskType.id === id)?.name
+  const { data: taskTypes } = useTaskTypes();
+  const name = taskTypes?.find((taskType) => taskType.id === id)?.name;
   if (!name) {
-    return null
+    return null;
   }
-  return <img alt={"未知"} src={name === "task" ? taskIcon : bugIcon}/>
-}
+  return <img alt={"未知"} src={name === "task" ? taskIcon : bugIcon} />;
+};
 
-const TaskCard = ({task}: {task: Task}) => {
-  const {startEdit} = useTasksModal()
-  const {name: keyword} = useTasksSearchParams()
-  return <Card onClick={() => startEdit(task.id)} key={task.id} style={{marginBottom: "0.5rem", cursor: "pointer"}}>
-    <Mark name={task.name} keyword={keyword} />
-    <TaskTypeIcon id={task.typeId} />
-  </Card>
-}
+const TaskCard = ({ task }: { task: Task }) => {
+  const { startEdit } = useTasksModal();
+  const { name: keyword } = useTasksSearchParams();
+  return (
+    <Card
+      onClick={() => startEdit(task.id)}
+      key={task.id}
+      style={{ marginBottom: "0.5rem", cursor: "pointer" }}
+    >
+      <Mark name={task.name} keyword={keyword} />
+      <TaskTypeIcon id={task.typeId} />
+    </Card>
+  );
+};
 
-export const KanbanColumn = ({kanban}: {kanban: Kanban}) => {
-  const {data: allTasks} = useTasks(useTasksSearchParams())
-  const tasks = allTasks?.filter(task => task.kanbanId === kanban.id)
-  
-  return <Container>
-    <Row between={true}>
-      <h3>{kanban.name}</h3>
-      <More kanban={kanban} />
-    </Row>
-    <TasksContainer>
-      {tasks?.map(task => (
-        <TaskCard task={task} key={task.id} />
-      ))}
-       <CreateTask kanbanId={kanban.id} />
-    </TasksContainer>
-  </Container>
-}
+export const KanbanColumn = React.forwardRef<
+  HTMLDivElement,
+  { kanban: Kanban }
+>(({ kanban, ...props }, ref) => {
+  const { data: allTasks } = useTasks(useTasksSearchParams());
+  const tasks = allTasks?.filter((task) => task.kanbanId === kanban.id);
 
-const More = ({kanban}: {kanban: Kanban}) => {
-  const {mutateAsync} = useDeleteKanban(useKanbansQueryKey())
+  return (
+    <Container {...props} ref={ref}>
+      <Row between={true}>
+        <h3>{kanban.name}</h3>
+        <More kanban={kanban} key={kanban.id} />
+      </Row>
+      <TasksContainer>
+        <Drop
+          type={"ROW"}
+          direction={"vertical"}
+          droppableId={"task" + kanban.id}
+        >
+          <DropChild>
+            {tasks?.map((task, taskIndex) => (
+              <Drag
+                key={task.id}
+                index={taskIndex}
+                draggableId={"task" + task.id}
+              >
+                <div>
+                  <TaskCard task={task} key={task.id} />
+                </div>
+              </Drag>
+            ))}
+          </DropChild>
+        </Drop>
+
+        <CreateTask kanbanId={kanban.id} />
+      </TasksContainer>
+    </Container>
+  );
+});
+
+const More = ({ kanban }: { kanban: Kanban }) => {
+  const { mutateAsync } = useDeleteKanban(useKanbansQueryKey());
   const startEdit = () => {
     Modal.confirm({
-      okText: '确定',
-      cancelText: '取消',
-      title: ' 确定删除看板吗',
-      onOk () {
-        return mutateAsync({id: kanban.id})
-      }
-    })
-  }
-  const overlay = () => <Menu>
-    <Menu.Item onClick={() =>startEdit()}>删除</Menu.Item>
-  </Menu>
-  return <Dropdown overlay={overlay}>
-    <Button type={'link'}>...</Button>
-  </Dropdown>
-}
+      okText: "确定",
+      cancelText: "取消",
+      title: " 确定删除看板吗",
+      onOk() {
+        return mutateAsync({ id: kanban.id });
+      },
+    });
+  };
+  const overlay = () => (
+    <Menu>
+      <Menu.Item onClick={() => startEdit()}>删除</Menu.Item>
+    </Menu>
+  );
+  return (
+    <Dropdown overlay={overlay}>
+      <Button type={"link"}>...</Button>
+    </Dropdown>
+  );
+};
 
 export const Container = styled.div`
   min-width: 27rem;
